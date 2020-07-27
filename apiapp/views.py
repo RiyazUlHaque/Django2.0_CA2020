85c9d55e900e31893e86d976f14ef764a177fd81
CodeCommit-at-966693457861 <riyazulhaque922@gmail.com>
2020-07-27 11:21:37 -0400

    Implementation of Django Template

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("HELLO WELCOME TO THE FIRST CUSTOM OUTPUT OF DJANGO APPLICATION")
+    context={
+        "data": "Hello I am doing something funny with my Django Template",
+        "list_Seq": [10,20,30,40,50,60,70,80,90,100]
+    }
+    return render(request,"index.html",context)
 
 def index1(request):
     return HttpResponse("HELLO I AM COMING FROM THE FUNCTION 2")
--- apiproject/settings.py
+++ apiproject/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
+print (TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'apiproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
